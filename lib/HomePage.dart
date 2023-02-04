5cd420a57becd2fb1f12cdaaf1be848261ceb916
Xeviousbr <xeviousbr@gmail.com>
2023-02-04 20:09:49 -0300

    Leitura do idEmpresa na tela Home

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,11 +13,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Inicializa();
     return Scaffold(
       appBar: AppBar(
-        title: Text('teletudo App'), 
+        title: Text('teletudo App'),
         centerTitle: true,
       ),
     );
+  }
+
+  Future<void> Inicializa() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? idEmpresa = prefs.getInt('idEmpresa');
+    print("idEmpresa = " + idEmpresa.toString());
   }
 }
