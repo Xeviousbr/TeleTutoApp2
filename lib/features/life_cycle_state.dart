e1a6dd572c05154144306ffc536c3b9d6e6b5df1
Xeviousbr <xeviousbr@gmail.com>
2023-03-19 23:15:44 -0300

    Coloquei a classe LifeCycle

---
+++ lib/features/life_cycle_state.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LifeCycleState extends StatefulWidget {
+  final Widget child;
+
+  const LifeCycleState({Key? key, required this.child}) : super(key: key);
+
+  @override
+  State<LifeCycleState> createState() => _LifeCycleStateState();
+}
+
+class _LifeCycleStateState extends State<LifeCycleState>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.resumed) {
+      sendStatusToServer('online');
+    } else if (state == AppLifecycleState.paused) {
+      sendStatusToServer('offline');
+    }
+  }
+
+  void sendStatusToServer(String status) async {
+    final deviceId = 'my_device_id';
+    final response = await http.post(
+      Uri.parse('http://your-server.com/api/status'),
+      body: {
+        'deviceId': deviceId,
+        'status': status,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      print('Failed to send status to server');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:tele_tudo_app/HomePage.dart';
 import 'LoginPage.dart';
+import 'features/life_cycle_state.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(),
+  runApp(LifeCycleState(
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    ),
   ));
 }
+
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: LoginPage(),
+//   ));
+// }
